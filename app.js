c184d7e08547f352e56a24041439b3992ce1b10e
Eren <117864036+Elcieren@users.noreply.github.com>
2023-01-09 18:56:37 +0300

    DropDown Button List -JavaScript
    İşe yaraya bileceğinizi düşündüğüm ufak bir yapıyı sizlerle paylaşıyorum ödevlerinizde veya projelerinizin küçük bir tarafında kullanabilirsiniz. Ayrıca buttonlar boş durmasın diye extra bir javascript kodu ile aktif hale getirdim

---
+++ app.js
@@ -0,0 +1,9 @@
+let click=document.querySelector('.click');
+          let list=document.querySelector('.list');
+          click.addEventListener("click", ()=>{
+          list.classList.toggle('newlist');
+          });
+
+  
+  
+  
